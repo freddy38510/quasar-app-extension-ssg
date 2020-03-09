9a33871530d7045ccdfc1807cc46605027b788ba
freddy38510 <freddyescobar@hotmail.fr>
2020-03-09 20:20:51 +0100

    Implement uninstall

--- src/uninstall.js
+++ src/uninstall.js
@@ -5,6 +5,9 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/UninstallAPI.js
  */
 
-module.exports = function (/* api */) {
-  //
+const rimraf = require('rimraf')
+
+module.exports = function (api) {
+  rimraf.sync(api.resolve.src('boot/bodyClasses.js'))
+  rimraf.sync(api.resolve.app('src-ssg'))
 }
