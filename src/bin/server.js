19083d9418512b6ba1abf1f4934fbdaebab70b27
Freddy <freddyescobar@hotmail.fr>
2021-10-16 17:00:26 +0200

    fix(deps): update dependency open to v8.3.0

--- src/bin/server.js
+++ src/bin/server.js
@@ -378,7 +378,7 @@ module.exports = (api) => {
         || !process.stdout.isTTY
       );
       if (!isMinimalTerminal) {
-        require('open')(fullUrl, { url: true });
+        esmRequire('open')(fullUrl);
       }
     }
   });
