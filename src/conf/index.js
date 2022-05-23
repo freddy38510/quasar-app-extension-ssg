2ac7f86f0c45962d0e3bcacbde5fa0f8c8259a53
Freddy <freddyescobar@hotmail.fr>
2022-05-23 15:40:31 +0200

    fix(dev): do not let Quasar set webpackConf when watching for changes

--- src/conf/index.js
+++ src/conf/index.js
@@ -4,6 +4,12 @@ const requireFromApp = require('../helpers/require-from-app');
 
 const QuasarConfFile = requireFromApp('@quasar/app/lib/quasar-conf-file');
 
+function encode(obj) {
+  return JSON.stringify(obj, (_, value) => (typeof value === 'function'
+    ? `/fn(${value.toString()})`
+    : value));
+}
+
 module.exports = class ExtendedQuasarConfFile extends QuasarConfFile {
   constructor(ctx, opts = {}) {
     super(ctx, opts);
@@ -20,11 +26,15 @@ module.exports = class ExtendedQuasarConfFile extends QuasarConfFile {
   }
 
   async compile() {
+    const oldWatch = this.watch;
+
     extendQuasarConf(this.sourceCfg);
 
     this.webpackConfChanged = false;
 
-    this.ctx.modeName = 'ssr';
+    this.watch = false; // do not let Quasar set webpackConf prop
+
+    this.ctx.modeName = 'ssr'; // needed to set publicPath correctly
 
     await super.compile();
 
@@ -33,6 +43,29 @@ module.exports = class ExtendedQuasarConfFile extends QuasarConfFile {
     Object.assign(this.quasarConf.build.env, {
       MODE: this.ctx.modeName,
     });
+
+    this.watch = oldWatch;
+
+    // If watching for changes then determine the type of them (webpack or not).
+    // The snapshot below should only contain webpack config:
+    if (this.watch) {
+      const cfg = this.sourceCfg;
+
+      const newConfigSnapshot = [
+        cfg.build ? encode(cfg.build) : '',
+        cfg.ssr && cfg.ssr.pwa ? encode(cfg.ssr.pwa) : '',
+        cfg.framework ? cfg.framework.autoImportComponentCase : '',
+        cfg.devServer ? encode(cfg.devServer) : '',
+        cfg.pwa ? encode(cfg.pwa) : '',
+        cfg.htmlVariables ? encode(cfg.htmlVariables) : '',
+      ].join('');
+
+      if (this.oldConfigSnapshot) {
+        this.webpackConfChanged = newConfigSnapshot !== this.oldConfigSnapshot;
+      }
+
+      this.oldConfigSnapshot = newConfigSnapshot;
+    }
   }
 
   async addWebpackConf() {
