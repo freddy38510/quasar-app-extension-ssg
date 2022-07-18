13aaa6f803962eb9511303b3e914f45e6d0b5ee5
Freddy <freddyescobar@hotmail.fr>
2022-07-18 16:11:24 +0200

    fix(install): add missing compatibility checks

--- src/install.js
+++ src/install.js
@@ -5,9 +5,26 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
 module.exports = function install(api) {
-  api.compatibleWith('quasar', '^2.0.0');
+  if (api.hasVite) {
+    api.compatibleWith('@quasar/app-vite', '^1.0.0');
+    api.compatibleWith('quasar', '>= 2.6.0');
+  } else {
+    api.compatibleWith('quasar', '^2.0.0');
 
-  api.compatibleWith('@quasar/app', '^3.0.0');
+    if (api.hasPackage('@quasar/app-webpack')) {
+      api.compatibleWith('@quasar/app-webpack', '^3.0.0');
+    } else {
+      api.compatibleWith('@quasar/app', '^3.0.0');
+    }
+
+    if (api.hasPackage('@quasar/app-webpack', '>= 3.4.0')) {
+      api.compatibleWith('quasar', '>= 2.6.0');
+    }
+
+    if (api.hasPackage('quasar', '>= 2.6.0')) {
+      api.compatibleWith('@quasar/app-webpack', '>= 3.4.0');
+    }
+  }
 
   let command = '$ quasar ssg generate';
 
