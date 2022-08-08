6fa701ee947d5bbf3bc88aebfe3197022140ed8e
Freddy <freddyescobar@hotmail.fr>
2022-08-08 18:27:25 +0200

    chore(commit-and-tag-version): fix configuration file to correctly bump files

--- .versionrc.js
+++ .versionrc.js
@@ -1,16 +1,28 @@
-const demos = {
-  filename: 'demos/quasar-ssg-vite/package.json',
-  updater: {
-    writeVersion: function (contents, version) {
-      const json = JSON.parse(contents);
-
-      json.devDependencies.['quasar-app-extension-ssg'] = version;
-
-      return JSON.stringify(json, null, 2);
-    }
-  }
-}
-
 module.exports = {
-  bumpFiles: [demos],
-}
+  bumpFiles: [
+    {
+      filename: "demos/quasar-ssg-vite/package.json",
+      updater: {
+        readVersion: (contents) => {
+          console.log(
+            JSON.parse(contents).devDependencies["quasar-app-extension-ssg"]
+          );
+          return JSON.parse(contents).devDependencies[
+            "quasar-app-extension-ssg"
+          ];
+        },
+        writeVersion: (contents, version) => {
+          const json = JSON.parse(contents);
+
+          json.devDependencies["quasar-app-extension-ssg"] = version;
+
+          return JSON.stringify(json, null, 2);
+        },
+      },
+    },
+    {
+      filename: "package.json",
+      type: "json",
+    },
+  ],
+};
