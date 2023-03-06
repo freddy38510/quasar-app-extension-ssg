e42e8024c4b0091dcf7caa45f7e40d6effd28777
Freddy <freddyescobar@hotmail.fr>
2023-03-06 16:48:01 +0100

    fix(vite): add the application URL to the printed dev banner

--- src/vite/helpers/print-dev-banner.js
+++ src/vite/helpers/print-dev-banner.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-void */
 /* eslint-disable no-console */
 
 const { requireFromApp } = require('./packages');
@@ -9,13 +10,29 @@ const {
 const { green, dim } = requireFromApp('kolorist');
 
 const greenBanner = green('»');
+const cache = {};
+
+function getIPList() {
+  // expensive operation, so cache the response
+  if (cache.ipList === void 0) {
+    const { getIPs } = requireFromApp('@quasar/app-vite/lib/helpers/net');
+    cache.ipList = getIPs().map((ip) => (ip === '127.0.0.1' ? 'localhost' : ip));
+  }
+
+  return cache.ipList;
+}
 
 function getBanner(quasarConf) {
   const { ctx } = quasarConf;
 
+  const urlList = quasarConf.devServer.host === '0.0.0.0'
+    ? getIPList().map((ip) => green(quasarConf.metaConf.getUrl(ip))).join('\n                           ')
+    : green(quasarConf.metaConf.APP_URL);
+
   const banner = [
     ` ${greenBanner} Reported at............ ${dim(new Date().toLocaleDateString())} ${dim(new Date().toLocaleTimeString())}`,
     ` ${greenBanner} App dir................ ${green(appPaths.appDir)}`,
+    ` ${greenBanner} App URL................ ${urlList}`,
     ` ${greenBanner} Dev mode............... ${green(ctx.modeName + (ctx.mode.pwa ? ' + pwa' : ''))}`,
     ` ${greenBanner} Pkg quasar............. ${green(`v${quasarVersion}`)}`,
     ` ${greenBanner} Pkg @quasar/app-vite... ${green(`v${cliAppVersion}`)}`,
